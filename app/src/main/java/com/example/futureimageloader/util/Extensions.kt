4dc7db3ca0f5c414ccd7958afbb05f298f3fe659
Hanhan Husna Firmansyah <hanhan.firmanysah@gdn-commerce.com>
2021-01-24 21:28:54 +0700

    glide module

--- app/src/main/java/com/example/futureimageloader/util/Extensions.kt
+++ app/src/main/java/com/example/futureimageloader/util/Extensions.kt
@@ -1,10 +1,12 @@
 package com.example.futureimageloader.util
 
 import android.widget.ImageView
-import com.bumptech.glide.Glide
+import com.example.futureimageloader.R
 
 fun ImageView.loadImageFromUrl(url: String) {
-    Glide.with(context)
+    GlideApp.with(context)
         .load(url)
+        .placeholder(R.drawable.ic_launcher_background)
+        .error(R.drawable.ic_baseline_broken_image_24)
         .into(this)
 }
